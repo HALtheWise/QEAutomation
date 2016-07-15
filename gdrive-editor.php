4b2f3b60010d2795975deccebf9aa2518fae5ee5
Eric Miller <eric@legoaces.org>
2016-07-14 22:47:20 -0400

    Added support for Overleaf

--- gdrive-editor.php
+++ gdrive-editor.php
@@ -43,6 +43,10 @@
 		if (mode == 'otherPDF') {
 			return 'https://docs.google.com/gview?url=~URL~&embedded=true'.replace('~URL~', encodeURIComponent(input));
 		}
+		if (mode == 'overleafPDF') {
+			var pdfURL = 'https://www.overleaf.com/docs/~ID~/pdf'.replace('~ID~', extractDocumentId(input));
+			return 'https://docs.google.com/gview?url=~URL~&embedded=true'.replace('~URL~', encodeURIComponent(pdfURL));
+		}
 		if (mode == 'googSheet') {
 			return 'https://docs.google.com/spreadsheets/d/~ID~/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false'.replace('~ID~', extractDocumentId(input));
 		}
@@ -59,7 +63,11 @@
 			return "Don't forget to publish the sheet through File...Publish to Web...Publish in order for the embed to succeed."
 		}
 		if (mode == 'sharePDF'){
+			// NOTE: Because this procedure is such a pain, ShareLatex embedding is disabled currently.
 			return "In order to update the published version, click \"Compile\" in a ShareLatex window while <i>not</i> signed in."
+		}
+		if (mode == 'overleafPDF'){
+			return "This embed will update automatically whenever any changes are made to the source document."
 		}
 		if (mode == 'googForm'){
 			return "Be sure to copy the URL from the form preview or published form, not the editor."
@@ -70,7 +78,7 @@
 	$(document).ready(function() {$('#embedSource').change(function() {$('#warning').html(getHelpText($('#embedSource').val()))})})
 
 	function extractDocumentId(input) {
-		var result = input.match('[0-9a-zA-Z-]{16,}'); 
+		var result = input.match('[0-9a-zA-Z-]{13,}'); 
 		if (result) {
 			return result[0];
 		}
@@ -83,6 +91,7 @@
 	<option value="googPDF">PDF from Google Drive</option>
 	<!-- <option value="sharePDF">PDF from ShareLatex</option> -->
 	<option value="otherPDF">PDF from another source</option>
+	<option selected="selected" value="overleafPDF">Compiled Overleaf document</option>
 	<option value="googSheet">Google Sheets (view only)</option>
 	<option value="googForm">Google Form</option>
 </select>
@@ -99,9 +108,9 @@
 <div class="additionalOptions">
 <p>Additional options...</p>
 	<select class="" id='width'>
-		<option selected="selected">100% width</option>
-		<option>50% width</option>
-		<option>25% width</option>
+		<option value="100%" selected="selected">100% width</option>
+		<option value="50%">50% width</option>
+		<option value="25%">25% width</option>
 	</select>
 
 	<!-- <label for="height">Height</label> -->
