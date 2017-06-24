597b67bb505926581e29c373cbff01ad5b05f24e
root <root@s19341268.onlinehome-server.com>
2017-06-24 13:58:20 -0500

    Attempted to create automatic refresh

--- overleaf-redirect.php
+++ overleaf-redirect.php
@@ -7,13 +7,20 @@
 
 	// echo substr($xml, 2, 10);
 
-	$match = preg_match_all("([\d\w]{12})", $shared_url, $output_array, PREG_SET_ORDER);
+	$match = preg_match_all("([\d\w]{12,})", $shared_url, $output_array, PREG_SET_ORDER);
 
 	$id = $output_array[0][0];
  	echo "<br>";
 	echo $id;
 
-	// echo "<br>";
+	echo "<br>";
+
+	// Send a request for the server to render the file
+	$render_request = 'https://www.overleaf.com/docs/' . id . '/manual_render';
+	$curl = curl_init($render_request);
+	curl_setopt($curl, CURLOPT_POST, true);
+	curl_exec($curl);
+	curl_close($curl);
 
 	header( 'Location: ' . 'https://www.overleaf.com/docs/' . $id . '/pdf.pdf' ) ;
 ?>
