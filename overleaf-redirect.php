4b6d01f6082f42c6fb8473197fd29982f50fb885
root <root@s19341268.onlinehome-server.com>
2017-06-24 13:35:25 -0500

    Repaired Overleaf integration to match new (better) API they expose

--- overleaf-redirect.php
+++ overleaf-redirect.php
@@ -1,19 +1,19 @@
 <?php 
 	$shared_url = $_REQUEST["shared_url"];
 
-	// echo $shared_url;
+	echo $shared_url;
 
-	$xml = file_get_contents($shared_url);
+	// $xml = file_get_contents($shared_url);
 
 	// echo substr($xml, 2, 10);
 
-	$match = preg_match_all("/https:\/\/preview.overleaf.com:443\/public\/[^\.]+\.pdf/", $xml, $output_array, PREG_SET_ORDER);
+	$match = preg_match_all("([\d\w]{12})", $shared_url, $output_array, PREG_SET_ORDER);
 
-	$firstlink = $output_array[0][0];
- // 	echo "<br>";
-	// echo $firstlink;
+	$id = $output_array[0][0];
+ 	echo "<br>";
+	echo $id;
 
 	// echo "<br>";
 
-	header( 'Location: ' . $firstlink ) ;
+	header( 'Location: ' . 'https://www.overleaf.com/docs/' . $id . '/pdf.pdf' ) ;
 ?>
